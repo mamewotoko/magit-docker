953e9e27f763a5c85793bb23b52f12ba073e8f8e
Takashi Masuyama <mamewotoko@gmail.com>
2020-07-29 20:41:38 +0900

    modify run script to start as docker container not docker-compose

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-SERVICE=magit
+#SERVICE=magit
 # SERVICE=magit-slim
+IMAGE_NAME=magit-docker:slim
+USER="$(id -u):$(id -g)"
 
-LINES="$(stty size | awk '{print $1}')" COLUMNS="$(stty size | awk '{print $2}')" docker-compose run $SERVICE
+LINES="$(stty size | awk '{print $1}')" COLUMNS="$(stty size | awk '{print $2}')" docker run -it -u $USER -v "$(git rev-parse --show-toplevel):/gitrepo" -v $HOMR/.ssh:/root/.ssh $IMAGE_NAME
